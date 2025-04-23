f81a696105cca450ad986cb2242c0ddc0f8f81ed
Eduardo Espiritu <eduardoespiritut@hotmail.com>
2025-04-23 09:33:26 -0600

    Create LaloSoul.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/LaloSoul.cs
@@ -0,0 +1,58 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+
+//https://dotnet.microsoft.com/es-es/languages/csharp
+
+//Comentarios en una linea
+
+/*
+Comentarios 
+en 
+multiples
+lineas
+*/
+///<summary>
+//Comentarios para documentacion XML
+///</summary>
+/// <param name="a">Primer número entero.</param>
+/// <param name="b">Segundo número entero.</param>
+/// <returns>La suma de dos números.</returns>
+
+int variable1NumericaEntera;
+
+const int constante1NumericaEntera = 1;
+
+//Tipos de datos primitivos
+int numericoEntero = 1;
+long numericoEnteroLargo =1111111111111111111111111;
+float dePuntoFlotante = 1.1;
+double dePuntoFlotanteDoblePresision = 1.111111;
+decial decimalesM = 111.11111111111111111m; /*Necesita un sufijo m*/
+char caracteres = 'H';
+string cadenas ="Hola Mundo";
+bool boleanos = true;
+
+
+
